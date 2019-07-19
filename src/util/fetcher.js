9c052e9e5abc7eadb8c069dacf020d3378655c44
Shawon Ashraf <shawon13@live.com>
2019-07-20 01:38:56 +0600

    deleted fetcher.js

--- src/util/fetcher.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export const fetchNowPlaying = async () => {
-  const url = `${process.env.REACT_APP_API_ROOT_URL}/core/v1/schedule/nowplaying`;
-  const response = await axios.get(url);
-  return response.data.nowPlaying;
-};
