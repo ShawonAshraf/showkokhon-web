52b019a9a5a980ef7d805d6989b7fce6f0ceae0e
Shawon Ashraf <shawon13@live.com>
2019-07-20 00:20:50 +0600

    state and update hook for Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 
 const Home = () => {
+  // state hook
+  const [nowPlaying, setNowPlaying] = useState([]);
+
+  // effect hook
+  useEffect(() => {
+    const fetchNowPlaying = async () => {
+      const url = `${process.env.REACT_APP_API_ROOT_URL}/core/v1/schedule/nowplaying`;
+      const response = await axios.get(url);
+
+      // set state
+      setNowPlaying(response.data.nowPlaying);
+    };
+
+    // call
+    fetchNowPlaying();
+  }, []);
+
   return (
     <div>
       <h1>Showkokhon</h1>
+      {nowPlaying.length === 0 ? <p>No movies are playing right now!</p> : <ul>
+        {nowPlaying.map(movie => <li key={movie}>{movie}</li>)}
+      </ul>}
     </div>
   );
 };
