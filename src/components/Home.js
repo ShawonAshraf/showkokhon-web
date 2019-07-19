1593662d6ab3d8f3e9836a7c823276a95be00593
Shawon Ashraf <shawon13@live.com>
2019-07-20 03:17:25 +0600

    NowPlaying Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import axios from "axios";
+import React from 'react';
+
+import NowPlaying from './NowPlaying';
 
 const Home = () => {
-  // state hook
-  const [nowPlaying, setNowPlaying] = useState([]);
-
-  // effect hook
-  useEffect(() => {
-    const fetchNowPlaying = async () => {
-      const url = `${process.env.REACT_APP_API_ROOT_URL}/core/v1/schedule/nowplaying`;
-      const response = await axios.get(url);
-
-      // set state
-      setNowPlaying(response.data.nowPlaying);
-    };
-
-    // call
-    fetchNowPlaying();
-  }, []);
-
   return (
     <div>
       <h1>Showkokhon</h1>
-      {nowPlaying.length === 0 ? <p>Loading!</p> : <ul>
-        {
-          nowPlaying.map(movie => <li key={movie}>
-            <a href={`schedule/${movie}`}>{movie}</a>
-          </li>)
-        }
-      </ul>}
+      <NowPlaying />
     </div>
   );
 };
---
+++ src/components/NowPlaying.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+
+import MovieCard from './MovieCard';
+
+const NowPlaying = () => {
+  // state hook
+  const [nowPlaying, setNowPlaying] = useState([]);
+
+  // effect hook
+  useEffect(() => {
+    const fetchNowPlaying = async () => {
+      const url = `${process.env.REACT_APP_API_ROOT_URL}/core/v1/schedule/nowplaying`;
+      const response = await axios.get(url);
+
+      // set state
+      setNowPlaying(response.data.nowPlaying);
+    };
+
+    // call
+    fetchNowPlaying();
+  }, []);
+
+  return (
+    <div>
+      {nowPlaying.length === 0 ? <p>Loading!</p> : <ul>
+        {
+          nowPlaying.map((res, index) => <li key={index}>
+            <MovieCard
+              key={index}
+              name={res._id.name}
+              imageUrl={res._id.imageUrl}
+            />
+          </li>)
+        }
+      </ul>}
+    </div>
+  );
+};
+
+export default NowPlaying;
