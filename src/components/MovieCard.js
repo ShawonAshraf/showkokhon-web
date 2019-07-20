b599afe266851491ad9f8f96fa7acf7e4cf1779a
Shawon Ashraf <shawon13@live.com>
2019-07-20 12:33:12 +0600

    moviecard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,61 @@
 import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
+import DefaultImage from '../images/default.jpg';
+
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 345,
+  },
+});
+
+// a cardview containing the name and cover art image of a movie
 const MovieCard = (props) => {
-  const { name, imageUrl } = props;
+  let {name, imageUrl} = props;
+  const classes = useStyles();
+
+  // use the default image in case missing
+  if (!imageUrl) {
+    imageUrl = DefaultImage;
+  }
+  console.log(imageUrl);
+
   return (
-    <div>
-      <a href={`schedule/${name}`}>{name}</a>
-      {/*TODO: add image*/}
-    </div>
-  )
-}
+    <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt={`${name}-cover`}
+          height="140"
+          image={imageUrl}
+          title={name}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {name}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <a href={`schedule/${name}`}>
+          <Button
+            size="small"
+            color="primary"
+            path={`schedule/${name}`}
+          >
+            Schedule
+          </Button>
+        </a>
+      </CardActions>
+    </Card>
+  );
+};
 
 export default MovieCard;
