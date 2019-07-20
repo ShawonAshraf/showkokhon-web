97832f971d453e5e69140dd40c807d902a0372f1
Shawon Ashraf <shawon13@live.com>
2019-07-20 13:55:12 +0600

    show time grid item

--- src/components/ShowTimeCard.js
+++ src/components/ShowTimeCard.js
@@ -1,14 +1,34 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
 const ShowTimeCard = (props) => {
   const { showTimes } = props;
+  const classes = useStyles();
 
   return (
-    <div>
-      <ul>
+    <div className={classes.root}>
+      <Grid container direction={'row'} spacing={3}>
         {
-          showTimes.map((showTime, index) => <li key={index}>{showTime}</li>)
+          showTimes.map((showTime, index) => <Grid key={index} item xs={6} md={3}>
+            <Paper className={classes.paper}>{showTime}</Paper>
+            </Grid>)
         }
+      </Grid>
+      <ul>
+
       </ul>
     </div>
   );
