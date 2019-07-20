7ed349576defb208ede70a0055a97f897a5c5569
Shawon Ashraf <shawon13@live.com>
2019-07-20 13:38:47 +0600

    added home button

--- src/components/GlobalAppBar.js
+++ src/components/GlobalAppBar.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -23,6 +24,7 @@ const GlobalAppBar = () => {
           <Typography variant="h5" className={classes.title}>
             Showkokhon
           </Typography>
+          <Button color="inherit" onClick={() => window.location.href= '/' }>Home</Button>
         </Toolbar>
       </AppBar>
     </div>
