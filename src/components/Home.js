0b643f8f15aed94b625c80650003a7b6fcbecc91
Shawon Ashraf <shawon13@live.com>
2019-08-17 12:42:08 +0600

    getComponentBasedOnStatusCode

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,12 +4,29 @@ import axios from 'axios';
 
 import NowPlaying from './NowPlaying';
 import ServiceNotAvailable from './ServiceNotAvailable';
+import ResourceNotAvailable from './ResourceNotAvailable';
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
   }
 }));
+
+const getComponentBasedOnStatusCode = (code) => {
+  switch (code) {
+    case 200:
+      return <NowPlaying />;
+
+    case 404:
+      return <ResourceNotAvailable
+        message={'Our databases have nothing to show at the moment sadly. :('}
+      />;
+    case 505:
+      return <ServiceNotAvailable />;
+    default:
+      return <p>Loading!</p>
+  }
+};
 
 const Home = () => {
   const classes = useStyles();
@@ -30,11 +47,9 @@ const Home = () => {
 
   return (
     <div className={classes.root}>
-      {
-        apiStatus === 200 ? <NowPlaying /> : <ServiceNotAvailable />
-      }
+      {getComponentBasedOnStatusCode(apiStatus)}
     </div>
-  );
+  )
 };
 
 export default Home;
